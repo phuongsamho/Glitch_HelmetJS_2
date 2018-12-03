490374a06e4fe375cb870f9d62c651cf96670509
Glitch (nebula-bill) <none>
2018-12-03 23:38:49 +0000

    📮🐏 Updated with Glitch

--- server.js
+++ server.js
@@ -15,15 +15,21 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
-
-
+//Hash and compare passwords asynchronously
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  console.log(hash);
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    console.log(res); //true  
+  });
+});
 //END_ASYNC
 
+
 //START_SYNC
-
-
-
+//Hash and compare passwords synchronously
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 //END_SYNC
 
 
